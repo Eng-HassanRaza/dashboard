4051600c004cb198e6b1ef5d2c5550aee0dc0784
hassan-raza <hassan.numl2013@gmail.com>
2021-01-27 00:16:12 +0500

    static 404 error v1

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include  # add this
 
@@ -11,4 +12,4 @@ urlpatterns = [
     path("", include("authentication.urls")), # Auth routes - login / register
     path("dashboard", include("app.urls")),             # UI Kits Html files
     path("", include("main.urls"))             # UI Kits Html files
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
